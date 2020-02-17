2bcdfb4ccfd6597eb9b5a410ae76322b64b9dcfa
Andy Mantell <andy.mantell@surevine.com>
2020-02-17 15:46:35 +0000

    Paring back create-react-app and initialising govuk-react-jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Template } from 'govuk-react-jsx';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { HelloWorld } from './HelloWorld';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Template>
+        <Route exact path="/">
+          <HelloWorld />
+        </Route>
+      </Template>
+    </Router>
   );
 }
 
---
+++ src/HelloWorld.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function HelloWorld() {
+  return <>
+    <h1 className="govuk-heading-l">Hello world</h1>
+    <p className="govuk-body">Hello world</p>
+  </>
+}
+
+export { HelloWorld }
